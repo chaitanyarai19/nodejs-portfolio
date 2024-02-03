b2461048e9d53237797ae723217cbd589569d1d2
chaitanyarai19 <chaitanyarai19@gmail.com>
2024-02-03 22:10:09 +0530

    latest updates

--- src/app.js
+++ src/app.js
@@ -2,13 +2,25 @@ const express = require('express');
 const hbs = require('hbs');
 const app = express();
 const routes = require('./routes/main');
+const mongoose = require('mongoose');
 
+mongoose.connect("mongodb://localhost/nodejs_portfolio")
+    .then(()=>{
+        console.log("Database Connected");
+    })
+    .catch((error)=>{
+        console.log("Error Connecting to Database");
+    })
+
+app.use(express.static("public")); 
 app.use('',routes);
 
 //template engine
 
 app.set('view engine','hbs');
-app.set('views','views')
+app.set('views','views');
+
+hbs.registerPartials("views/partials");
 
 app.listen(process.env.PORT || 2000,()=>{
     console.log("server started");
