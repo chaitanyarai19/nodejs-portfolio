970900498a84ed4f4e70584008f7416cc3bc9fa7
chaitanyarai19 <chaitanyarai19@gmail.com>
2024-02-03 20:08:16 +0530

    updated code

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-console.log("Hello");
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+const routes = require('./routes/main');
+
+app.use('',routes);
+
+//template engine
+
+app.set('view engine','hbs');
+app.set('views','views')
+
+app.listen(process.env.PORT || 2000,()=>{
+    console.log("server started");
+});
